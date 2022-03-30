a6fd7c7227d031d89480510676fecf6e430769aa
Samim sargin Ozcan <saozcan@k1m33s01.42istanbul.com.tr>
2022-03-31 00:50:08 +0300

    last

--- ft_printf/ft_printf.h
+++ ft_printf/ft_printf.h
@@ -1,34 +1,36 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: saozcan <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/02/02 09:41:51 by saozcan           #+#    #+#             */
-/*   Updated: 2022/02/02 13:33:10 by saozcan          ###   ########.fr       */
+/*   Created: 2022/02/07 13:12:20 by saozcan           #+#    #+#             */
+/*   Updated: 2022/02/09 19:09:46 by saozcan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef GET_NEXT_LINE_H
+#ifndef FT_PRINTF_H
 
-# define GET_NEXT_LINE_H
+# define FT_PRINTF_H
 
-# include <stdlib.h>
 # include <stdio.h>
+# include <stdarg.h>
 # include <unistd.h>
-# include <fcntl.h>
-# include <string.h>
+# include <stdlib.h>
 
-# ifndef BUFFER_SIZE
-#  define BUFFER_SIZE 42
+# ifndef BASE
+#  define BASE "0123456789abcdef"
 # endif
 
-char	*ft_strdup(char *s, int start, size_t len);
-size_t	ft_strlen_new(char *s, int c);
-void	ft_memcpy(char *str, int i);
-char	*ft_strchr(char *str, int c);
-char	*ft_strjoin(char *s1, char *s2);
-char	*get_next_line(int fd);
+# ifndef BASEV2
+#  define BASEV2 "0123456789ABCDEF"
+# endif
+
+void	ft_putchar(char c, int *count);
+void	ft_putstr(const char *str, int *count);
+void	ft_putnbr(long double nbr, int base, int *count);
+void	ft_section_part(va_list lst, const char str, int *count);
+int		ft_printf(const char *str, ...);
 
 #endif
